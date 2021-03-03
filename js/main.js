d31bd03350e2b3f141493e527d20b3ad51e57a19
kevin-shen-nti-johanneberg <74763174+kevin-shen-nti-johanneberg@users.noreply.github.com>
2021-03-03 22:40:53 +0100

    arbete
    mitt arbete

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-let button = document.querySelector("uni-button")
+
+let button = document.querySelector(".uni-button").addEventListener("click",showMenu)
+let layer = document.querySelector(".fade-layer").addEventListener("click",showMenu)
 
 function showMenu(){
-    let menu = document.querySelector("nav.uni-menu")
+    let menu = document.querySelector(".uni-menu")
     menu.classList.toggle("show")
 
     let layer = document.querySelector(".fade-layer")
